3ec2bb21fcac4529eae4beb1f470cfa1f5ee99bf
seraco <sebastiaramon_1994@hotmail.com>
2018-03-21 20:00:20 +0100

    CHeatConduction with comments

--- include/CHeatConduction.hpp
+++ include/CHeatConduction.hpp
@@ -1,3 +1,31 @@
+/*!
+ * @file CHeatConduction.hpp
+ * @brief Headers of the main subroutines for solving the heat conduction problem.
+ *        The implementation is in the <i>CHeatConduction.cpp</i> file.
+ * @author S.Ramon (seraco)
+ * @version 0.0.1
+ *
+ * Copyright 2018 S.Ramon
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef __CHEATCONDUCTION_HPP
 #define __CHEATCONDUCTION_HPP
 
@@ -6,38 +34,119 @@
 #include "CMatrix.hpp"
 #include "CMesh.hpp"
 
+/*!
+ * @class CHeatConduction
+ * @brief Class to solve the heat conduction problem on a plate.
+ */
 class CHeatConduction {
     private:
-        CMatrix Kee;
-        CMatrix Kff;
-        CMatrix Kef;
-        CMatrix Te;
-        CMatrix Tf;
-        CMatrix Fe;
-        CMatrix Ff;
-        CMatrix temp;
-        CMatrix flux;
+        CMatrix Kee;    /*!< @brief Conductance submatrix at known temperatures.*/
+        CMatrix Kff;    /*!< @brief Conductance submatrix at known fluxes.*/
+        CMatrix Kef;    /*!< @brief Conductance submatrix at known temperatures and fluxes.*/
+        CMatrix Te;     /*!< @brief Temperature subvector at known temperatures.*/
+        CMatrix Tf;     /*!< @brief Temperature subvector at known fluxes.*/
+        CMatrix Fe;     /*!< @brief Flux subvector at known temperatures.*/
+        CMatrix Ff;     /*!< @brief Flux subvector at known fluxes.*/
+        CMatrix temp;   /*!< @brief Temperature vector.*/
+        CMatrix flux;   /*!< @brief Flux vector.*/
 
+        /*!
+         * @brief Subroutine to subdivide matrices and vecors, Kee, Kff, Kef, Te, Tf.
+         * @param[in] bnd - Boundary conditions.
+         * @param[in] cnd - Conductance.
+         */
         void partitionMatrices(const CBoundaryConditions& bnd,
                                const CConductance& cnd);
+
+        /*!
+         * @brief Solve the heat problem returning the temperature vector.
+         * @param[in] msh - Mesh.
+         * @param[in] bnd - Boundary conditions.
+         * @return Temperature vector.
+         */
         CMatrix solveTemperature(const CMesh& msh,
                                  const CBoundaryConditions& bnd);
+
+        /*!
+         * @brief Solve the heat problem returning the flux vector.
+         * @param[in] msh - Mesh.
+         * @param[in] bnd - Boundary conditions.
+         * @return Flux vector.
+         */
         CMatrix solveFlux(const CMesh& msh, const CBoundaryConditions& bnd);
 
     public:
+        /*!
+         * @brief Constructor of the class.
+         */
         CHeatConduction();
+
+        /*!
+         * @brief Constructor of the class.
+         * @param[in] bnd - Boundary conditions.
+         * @param[in] cnd - Conductance.
+         * @param[in] msh - Mesh.
+         */
         CHeatConduction(const CBoundaryConditions& bnd, const CConductance& cnd,
                         const CMesh& msh);
+
+        /*!
+         * @brief Destructor of the class.
+         */
         virtual ~CHeatConduction();
 
+        /*!
+         * @brief Get Kee submatrix.
+         * @return Kee submatrix.
+         */
         CMatrix getKee() const;
+
+        /*!
+         * @brief Get Kff submatrix.
+         * @return Kff submatrix.
+         */
         CMatrix getKff() const;
+
+        /*!
+         * @brief Get Kef submatrix.
+         * @return Kef submatrix.
+         */
         CMatrix getKef() const;
+
+        /*!
+         * @brief Get Te subvector.
+         * @return Te subvector.
+         */
         CMatrix getTe() const;
+
+        /*!
+         * @brief Get Tf subvector.
+         * @return Tf subvector.
+         */
         CMatrix getTf() const;
+
+        /*!
+         * @brief Get Fe subvector.
+         * @return Fe subvector.
+         */
         CMatrix getFe() const;
+
+        /*!
+         * @brief Get Ff subvector.
+         * @return Ff subvector.
+         */
         CMatrix getFf() const;
+
+        /*!
+         * @brief Get temperature vector.
+         * @return Temperature vector.
+         */
         CMatrix getTemp() const;
+
+        /*!
+         * @brief Get flux vector.
+         * @return Flux vector.
+         */
         CMatrix getFlux() const;
 };
 
--- src/CHeatConduction.cpp
+++ src/CHeatConduction.cpp
@@ -1,3 +1,30 @@
+/*!
+ * @file CHeatConduction.cpp
+ * @brief The main subroutines for solving the heat conduction problem.
+ * @author S.Ramon (seraco)
+ * @version 0.0.1
+ *
+ * Copyright 2018 S.Ramon
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef __CHEATCONDUCTION_CPP
 #define __CHEATCONDUCTION_CPP
 
@@ -12,6 +39,7 @@ CHeatConduction::CHeatConduction() {}
 CHeatConduction::CHeatConduction(const CBoundaryConditions& bnd,
                                  const CConductance& cnd,
                                  const CMesh& msh) {
+    /*--- Solve heat conduction problem. ---*/
     partitionMatrices(bnd, cnd);
     temp = solveTemperature(msh, bnd);
     flux = solveFlux(msh, bnd);
@@ -50,6 +78,7 @@ CMatrix CHeatConduction::getFlux() const {
 
 void CHeatConduction::partitionMatrices(const CBoundaryConditions& bnd,
                                         const CConductance& cnd) {
+    /*--- Initialize variables to be used in the subroutine. ---*/
     unsigned nTNod = bnd.getNTempNodes();
     CMatrix tpNod = bnd.getTempNodes();
     unsigned rDof = bnd.getNReducedDof();
@@ -58,16 +87,19 @@ void CHeatConduction::partitionMatrices(const CBoundaryConditions& bnd,
     CMatrix f = bnd.getFluxBCVector();
     CMatrix K = cnd.getConducMtx();
 
+    /*--- Te subvector. ---*/
     Te = CMatrix(nTNod, 1, 0.0);
     for (unsigned i = 0; i < nTNod; i++) {
         Te(i, 0) =  T(tpNod(0, i), 0);
     }
 
+    /*--- Ff subvector. ---*/
     Ff = CMatrix(rDof, 1, 0.0);
     for (unsigned i = 0; i < rDof; i++) {
         Ff(i, 0) =  f(rDofVec(0, i), 0);
     }
 
+    /*--- Kee submatrix. ---*/
     Kee = CMatrix(nTNod, nTNod, 0.0);
     for (unsigned i = 0; i < nTNod; i++) {
         for (unsigned j = 0; j < nTNod; j++) {
@@ -75,6 +107,7 @@ void CHeatConduction::partitionMatrices(const CBoundaryConditions& bnd,
         }
     }
 
+    /*--- Kff submatrix. ---*/
     Kff = CMatrix(rDof, rDof, 0.0);
     for (unsigned i = 0; i < rDof; i++) {
         for (unsigned j = 0; j < rDof; j++) {
@@ -82,6 +115,7 @@ void CHeatConduction::partitionMatrices(const CBoundaryConditions& bnd,
         }
     }
 
+    /*--- Kef submatrix. ---*/
     Kef = CMatrix(nTNod, rDof, 0.0);
     for (unsigned i = 0; i < nTNod; i++) {
         for (unsigned j = 0; j < rDof; j++) {
@@ -92,6 +126,7 @@ void CHeatConduction::partitionMatrices(const CBoundaryConditions& bnd,
 
 CMatrix CHeatConduction::solveTemperature(const CMesh& msh,
                                           const CBoundaryConditions& bnd) {
+    /*--- Initialize variables to be used in the subroutine. ---*/
     unsigned nDof = msh.getNDofTotal();
     CMatrix T = CMatrix(nDof, 1, 0.0);
     unsigned nTNod = bnd.getNTempNodes();
@@ -99,14 +134,15 @@ CMatrix CHeatConduction::solveTemperature(const CMesh& msh,
     unsigned rDof = bnd.getNReducedDof();
     CMatrix rDofVec = bnd.getReducedDofVector();
 
+    /*--- Solve the linear system of equations. ---*/
     CMatrix RHS = Ff - Kef.transpose() * Te;
     CLinearSystem sys = CLinearSystem(Kff, RHS);
     Tf = sys.iterativeSolve();
 
+    /*--- Build global temperature vector combining Te and Tf. ---*/
     for (unsigned i = 0; i < nTNod; i++) {
         T(tpNod(0, i), 0) = Te(i, 0);
     }
-
     for (unsigned i = 0; i < rDof; i++) {
         T(rDofVec(0, i), 0) = Tf(i, 0);
     }
@@ -116,6 +152,7 @@ CMatrix CHeatConduction::solveTemperature(const CMesh& msh,
 
 CMatrix CHeatConduction::solveFlux(const CMesh& msh,
                                    const CBoundaryConditions& bnd) {
+    /*--- Initialize variables to be used in the subroutine. ---*/
     unsigned nDof = msh.getNDofTotal();
     CMatrix F = CMatrix(nDof, 1, 0.0);
     unsigned nTNod = bnd.getNTempNodes();
@@ -125,10 +162,10 @@ CMatrix CHeatConduction::solveFlux(const CMesh& msh,
 
     Fe = Kee * Te + Kef * Tf;
 
+    /*--- Build global flux vector combining Fe and Ff. ---*/
     for (unsigned i = 0; i < nTNod; i++) {
         F(tpNod(0, i), 0) = Fe(i, 0);
     }
-
     for (unsigned i = 0; i < rDof; i++) {
         F(rDofVec(0, i), 0) = Ff(i, 0);
     }
