eeca91fe52a05d849c23402791c128c38ca89639
seraco <sebastiaramon_1994@hotmail.com>
2018-03-21 21:52:27 +0100

    CMatrix with comments

--- include/CMatrix.hpp
+++ include/CMatrix.hpp
@@ -1,3 +1,31 @@
+/*!
+ * @file CMatrix.hpp
+ * @brief Headers of the main subroutines for defining matrices and their operations.
+ *        The implementation is in the <i>CMatrix.cpp</i> file.
+ * @author S.Ramon (seraco)
+ * @version 0.0.1
+ *
+ * Copyright 2018 S.Ramon
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef __CMATRIX_HPP
 #define __CMATRIX_HPP
 
@@ -10,51 +38,194 @@ extern "C" {
                          double* work, const int& lwork, int& info);
 }
 
+/*!
+ * @class CMatrix
+ * @brief Class to define matrices and their operations.
+ */
 class CMatrix {
     private:
-        unsigned nRows;
-        unsigned nCols;
-        double* mtx;
+        unsigned nRows;     /*!< @brief Number of rows.*/
+        unsigned nCols;     /*!< @brief Number of columns.*/
+        double* mtx;        /*!< @brief Pointer at the beginning of matrix entries.*/
 
     public:
+        /*!
+         * @brief Constructor of the class.
+         */
         CMatrix();
+
+        /*!
+         * @brief Constructor of the class.
+         * @param[in] rows - Number of rows.
+         * @param[in] cols - Number of columns.
+         * @param[in] initValue - Initial value to populate matrix.
+         */
         CMatrix(unsigned rows, unsigned cols, const double initValue);
+
+        /*!
+         * @brief Copy constructor of the class.
+         * @param[in] rhs - Matrix to copy.
+         */
         CMatrix(const CMatrix& rhs);
+
+        /*!
+         * @brief Destructor of the class.
+         */
         virtual ~CMatrix();
 
+        /*!
+         * @brief Get number of rows.
+         * @return Number of rows.
+         */
         unsigned getRows() const;
+
+        /*!
+         * @brief Get number of columns.
+         * @return Number of columns.
+         */
         unsigned getCols() const;
-        // unsigned isErrWithMtxSize();
+
+        /*!
+         * @brief Print matrix to the console.
+         */
         void printMtx();
+
+        /*!
+         * @brief Compute the transpose.
+         * @return Transpose of the matrix.
+         */
         CMatrix transpose();
-        // bool isLowerTriangular();
-        // bool isUpperTriangular();
+
+        /*!
+         * @brief Get the pointer at the beginning of the entries.
+         * @return Pointer at the beginning of the entries.
+         */
         double* getMtxAddress();
+
+        /*!
+         * @brief Construct a linear space between left and right.
+         * @param[in] left - Initial left node.
+         * @param[in] right - Final right node.
+         * @param[in] nNode - Number of nodes in the linspace.
+         * @return Linspace.
+         */
         static CMatrix linspace(const double left, const double right,
                                 const unsigned nNode);
+
+        /*!
+         * @brief Compute the determinant of the matrix.
+         * @return Determinant.
+         */
         double determinant();
+
+        /*!
+         * @brief Compute the inverse of the matrix.
+         * @return Inverse matrix.
+         */
         CMatrix inverse();
 
-        // Adress operator after type T is to be able to assign values when
-        // there is an assignment like A(i, j) = 3
+        /*!
+         * @brief Operator overloading of parenthesis to access entries.
+         * @param[in] i - Row number.
+         * @param[in] j - Column number.
+         * @return Entry of the matrix at row i and column j.
+         */
         double& operator()(const unsigned i, const unsigned j);
+
+        /*!
+         * @brief Operator overloading of parenthesis to access entries.
+         * @param[in] i - Row number.
+         * @param[in] j - Column number.
+         * @return Entry of the matrix at row i and column j.
+         */
         const double& operator()(const unsigned i, const unsigned j) const;
+
+        /*!
+         * @brief Operator overloading of equal sign.
+         * @param[in] rhs - RHS matrix to copy.
+         * @return Matrix with copied entries from rhs.
+         */
         CMatrix& operator=(const CMatrix& rhs);
-        // TODO: Throw error if matrices are of different size!
+
+        /*!
+         * @brief Operator overloading of plus sign to sum matrices.
+         * @param[in] rhs - RHS matrix to sum.
+         * @return Matrix with summed entries.
+         */
         CMatrix operator+(const CMatrix& rhs);
-        // TODO: Throw error if matrices are of different size!
+
+        /*!
+         * @brief Operator overloading of plus-equal sign to sum matrices.
+         * @param[in] rhs - RHS matrix to sum.
+         * @return Matrix with summed entries.
+         */
         CMatrix& operator+=(const CMatrix& rhs);
-        // TODO: Throw error if matrices are of different size!
+
+        /*!
+         * @brief Operator overloading of minus sign to substract matrices.
+         * @param[in] rhs - RHS matrix to substract.
+         * @return Matrix with substracted entries.
+         */
         CMatrix operator-(const CMatrix& rhs);
-        // TODO: Throw error if matrices are of different size!
+
+        /*!
+         * @brief Operator overloading of minus-equal sign to substract matrices.
+         * @param[in] rhs - RHS matrix to substract.
+         * @return Matrix with substracted entries.
+         */
         CMatrix& operator-=(const CMatrix& rhs);
-        // TODO: Throw error if matrices are of incorrect size!
+
+        /*!
+         * @brief Operator overloading of asterisk sign to multiply matrices.
+         * @param[in] rhs - RHS matrix to multiply.
+         * @return Product of the two matrices.
+         */
         CMatrix operator*(const CMatrix& rhs);
+
+        /*!
+         * @brief Operator overloading of asterisk-equal sign to multiply matrices.
+         * @param[in] rhs - RHS matrix to multiply.
+         * @return Product of the two matrices.
+         */
         CMatrix& operator*=(const CMatrix& rhs);
+
+        /*!
+         * @brief Operator overloading of plus sign to sum a constant to a matrix.
+         * @param[in] rhs - RHS constant to sum.
+         * @return Matrix with the constant summed.
+         */
         CMatrix operator+(const double& rhs);
+
+        /*!
+         * @brief Operator overloading of minus sign to substract a constant from
+         *        a matrix.
+         * @param[in] rhs - RHS constant to substract.
+         * @return Matrix with the constant substracted.
+         */
         CMatrix operator-(const double& rhs);
+
+        /*!
+         * @brief Operator overloading of asterisk sign to multiply a constant
+         *        with a matrix.
+         * @param[in] rhs - RHS constant to multiply.
+         * @return Matrix with the constant multiplied.
+         */
         CMatrix operator*(const double& rhs);
+
+        /*!
+         * @brief Operator overloading of slash sign to divide a constant with
+         *        a matrix.
+         * @param[in] rhs - RHS constant to divide.
+         * @return Matrix with the constant divided.
+         */
         CMatrix operator/(const double& rhs);
+
+        /*!
+         * @brief Operator overloading of power sign to take the power of each
+         *        matrix entry with the constant.
+         * @param[in] rhs - RHS constant to take the power.
+         * @return Matrix with each entry to the power of the constant.
+         */
         CMatrix operator^(const double& rhs);
 };
 
--- src/CMatrix.cpp
+++ src/CMatrix.cpp
@@ -1,3 +1,30 @@
+/*!
+ * @file CMatrix.cpp
+ * @brief The main subroutines for defining matrices and their operations.
+ * @author S.Ramon (seraco)
+ * @version 0.0.1
+ *
+ * Copyright 2018 S.Ramon
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef __CMATRIX_CPP
 #define __CMATRIX_CPP
 
@@ -7,15 +34,23 @@
 #include "../include/CMatrix.hpp"
 
 CMatrix::CMatrix() {
+    /*--- Initialize properties. ---*/
     nRows = 0;
     nCols = 0;
+
+    /*--- Allocate memory for the entries. ---*/
     mtx = new double[0];
 }
 
 CMatrix::CMatrix(unsigned rows, unsigned cols, const double initValue) {
+    /*--- Initialize properties. ---*/
     nRows = rows;
     nCols = cols;
+
+    /*--- Allocate memory for the entries. ---*/
     mtx = new double[cols*rows];
+
+    /*--- Initialize entries with initValue. ---*/
     for (unsigned j = 0; j < nCols; j++) {
         for (unsigned i = 0; i < nRows; i++) {
             mtx[j*nRows+i] = initValue;
@@ -24,9 +59,14 @@ CMatrix::CMatrix(unsigned rows, unsigned cols, const double initValue) {
 }
 
 CMatrix::CMatrix(const CMatrix& rhs) {
+    /*--- Initialize properties. ---*/
     nRows = rhs.nRows;
     nCols = rhs.nCols;
+
+    /*--- Allocate memory for the entries. ---*/
     mtx = new double[nCols*nRows];
+
+    /*--- Initialize entries with the same entries in rhs. ---*/
     for (unsigned j = 0; j < nCols; j++) {
         for (unsigned i = 0; i < nRows; i++) {
             mtx[j*nRows+i] = rhs.mtx[j*nRows+i];
@@ -35,6 +75,7 @@ CMatrix::CMatrix(const CMatrix& rhs) {
 }
 
 CMatrix::~CMatrix() {
+    /*--- Release the memory allocated for the entries. ---*/
     delete[] mtx;
 }
 
@@ -46,14 +87,10 @@ unsigned CMatrix::getCols() const {
     return nCols;
 }
 
-// template<typename T>
-// unsigned CMatrix<T>::isErrWithMtxSize() {
-//     return (mtx.size()!=nCols && mtx[0].size()!=nRows);
-// }
-
 CMatrix CMatrix::transpose() {
     CMatrix res(nCols, nRows, 0.0);
 
+    /*--- Change the i-j indices to obtain the transpose. ---*/
     for (unsigned j = 0; j < nCols; j++) {
         for (unsigned i = 0; i < nRows; i++) {
             res(j, i) = mtx[j*nRows+i];
@@ -66,8 +103,11 @@ CMatrix CMatrix::transpose() {
 CMatrix CMatrix::linspace(const double left, const double right,
                                  const unsigned nNode) {
     CMatrix res = CMatrix(1, nNode, 0.0);
+
+    /*--- Space separation between adjacent nodes. ---*/
     double deltaX = (right - left) / (nNode - 1);
 
+    /*--- Construct the linspace. ---*/
     for (unsigned i = 0; i < nNode; i++) {
         res(i, 0) = left + i*deltaX;
     }
@@ -76,29 +116,32 @@ CMatrix CMatrix::linspace(const double left, const double right,
 }
 
 double CMatrix::determinant() {
-    // TODO: Check for square matrix
+    /*--- Initialize variables to use in the subroutine. ---*/
     double res = 1.0;
     int info = 0;
     CMatrix A = CMatrix(*this);
     int* vPivPtr = new int[nCols];
     double* APtr = A.getMtxAddress();
+
+    /*--- LU decomposition of the matrix. ---*/
     F77NAME(dgetrf)(nCols, nCols, APtr, nCols, vPivPtr, info);
 
+    /*--- The determinant will be the product of the diagonal of U. ---*/
     for (unsigned j = 0; j < nCols; j++) {
         res *= A(j, j);
-        // std::cout << j << " " << vPivPtr[j] << std::endl;
         if (j != vPivPtr[j]) {
             res *= -1.0;
         }
     }
 
+    /*--- Release allocated memory. ---*/
     delete[] vPivPtr;
 
     return res;
 }
 
 CMatrix CMatrix::inverse() {
-    // TODO: Check for square matrix
+    /*--- Initialize variables to use in the subroutine. ---*/
     int info = 0;
     CMatrix A = CMatrix(*this);
     int* vPivPtr = new int[nCols];
@@ -106,36 +149,18 @@ CMatrix CMatrix::inverse() {
     int nWorkspace = nCols * nCols;
     double* workspace = new double[nWorkspace];
 
+    /*--- LU decomposition of the matrix. ---*/
     F77NAME(dgetrf)(nCols, nCols, APtr, nCols, vPivPtr, info);
+
+    /*--- LAPACK subroutine to compute the inverse. ---*/
     F77NAME(dgetri)(nCols, APtr, nCols, vPivPtr, workspace, nWorkspace, info);
 
+    /*--- Release allocated memory. ---*/
     delete[] vPivPtr;
     delete[] workspace;
 
     return A;
 }
-
-// template<typename T>
-// bool CMatrix<T>::isLowerTriangular() {
-//     for (unsigned j = 0; j < nCols; j++) {
-//         for (unsigned i = i+1; i < nRows; i++) {
-//             if(mtx[j][i] != 0.0) {
-//                 return false;
-//             }
-//         }
-//     }
-//     return true;
-// }
-
-// template<typename T>
-// bool CMatrix<T>::isUpperTriangular() {
-//     for (unsigned j = 1; j < nCols; j++) {
-//         for (unsigned i = 0; i < j; i++) {
-//             if(mtx[j][i] != 0.0) return false;
-//         }
-//     }
-//     return true;
-// }
 
 double* CMatrix::getMtxAddress() {
     return &mtx[0];
@@ -152,7 +177,6 @@ void CMatrix::printMtx() {
 }
 
 double& CMatrix::operator()(const unsigned i, const unsigned j) {
-    // std::cout << "Row: " << i << " Col: " << j << ": " << &mtx[i][j] << std::endl;
     return mtx[j*nRows+i];
 }
 
@@ -161,12 +185,19 @@ const double& CMatrix::operator()(const unsigned i, const unsigned j) const {
 }
 
 CMatrix& CMatrix::operator=(const CMatrix& rhs) {
+    /*--- If the matrix is the same, simply return the same matrix. ---*/
     if (&rhs == this) return *this;
 
+    /*--- Release allocated memory for entries in order to allocate new space for
+          different nRows and nCols. ---*/
     delete mtx;
     nRows = rhs.getRows();
     nCols = rhs.getCols();
+
+    /*--- Allocated memory for the new entries. ---*/
     mtx = new double[nCols*nRows];
+
+    /*--- Copy entries from the rhs to the current matrix. ---*/
     for (unsigned j = 0; j < nCols; j++) {
         for (unsigned i = 0; i < nRows; i++) {
             mtx[j*nRows+i] = rhs.mtx[j*nRows+i];
@@ -176,10 +207,10 @@ CMatrix& CMatrix::operator=(const CMatrix& rhs) {
     return *this;
 }
 
-// TODO: Throw error if matrices are of different size!
 CMatrix CMatrix::operator+(const CMatrix& rhs) {
     CMatrix res(nRows, nCols, 0.0);
 
+    /*--- Sum every entry. ---*/
     for (unsigned j = 0; j < nCols; j++) {
         for (unsigned i = 0; i < nRows; i++) {
             res(i, j) = mtx[j*nRows+i] + rhs(i, j);
@@ -189,17 +220,17 @@ CMatrix CMatrix::operator+(const CMatrix& rhs) {
     return res;
 }
 
-// TODO: Throw error if matrices are of different size!
 CMatrix& CMatrix::operator+=(const CMatrix& rhs) {
+    /*--- Use the + overloaded sign to compute +=. ---*/
     CMatrix res = (*this) + rhs;
     (*this) = res;
     return *this;
 }
 
-// TODO: Throw error if matrices are of different size!
 CMatrix CMatrix::operator-(const CMatrix& rhs) {
     CMatrix res(nRows, nCols, 0.0);
 
+    /*--- Substract every entry. ---*/
     for (unsigned j = 0; j < nCols; j++) {
         for (unsigned i = 0; i < nRows; i++) {
             res(i, j) = mtx[j*nRows+i] - rhs(i, j);
@@ -209,18 +240,18 @@ CMatrix CMatrix::operator-(const CMatrix& rhs) {
     return res;
 }
 
-// TODO: Throw error if matrices are of different size!
 CMatrix& CMatrix::operator-=(const CMatrix& rhs) {
+    /*--- Use the - overloaded sign to compute -=. ---*/
     CMatrix res = (*this) - rhs;
     (*this) = res;
     return *this;
 }
 
 CMatrix CMatrix::operator*(const CMatrix& rhs) {
-    // unsigned rhsRows = rhs.getCols();
     unsigned rhsCols = rhs.getCols();
     CMatrix res(nRows, rhsCols, 0.0);
 
+    /*--- Compute the product of matrices. ---*/
     for (unsigned j = 0; j < rhsCols; j++) {
         for (unsigned i = 0; i < nRows; i++) {
             for (unsigned k = 0; k < nCols; k++) {
@@ -233,6 +264,7 @@ CMatrix CMatrix::operator*(const CMatrix& rhs) {
 }
 
 CMatrix& CMatrix::operator*=(const CMatrix& rhs) {
+    /*--- Use the * overloaded sign to compute *=. ---*/
     CMatrix res = (*this) * rhs;
     (*this) = res;
     return *this;
@@ -241,6 +273,7 @@ CMatrix& CMatrix::operator*=(const CMatrix& rhs) {
 CMatrix CMatrix::operator+(const double& rhs) {
     CMatrix res(nRows, nCols, 0.0);
 
+    /*--- Sum every entry with the constant. ---*/
     for (unsigned j = 0; j < nCols; j++) {
         for (unsigned i = 0; i < nRows; i++) {
             res(i, j) = mtx[j*nRows+i] + rhs;
@@ -254,6 +287,7 @@ CMatrix CMatrix::operator+(const double& rhs) {
 CMatrix CMatrix::operator-(const double& rhs) {
     CMatrix res(nRows, nCols, 0.0);
 
+    /*--- Substract every entry with the constant. ---*/
     for (unsigned j = 0; j < nCols; j++) {
         for (unsigned i = 0; i < nRows; i++) {
             res(i, j) = mtx[j*nRows+i] - rhs;
@@ -267,6 +301,7 @@ CMatrix CMatrix::operator-(const double& rhs) {
 CMatrix CMatrix::operator*(const double& rhs) {
     CMatrix res(nRows, nCols, 0.0);
 
+    /*--- Multiply every entry with the constant. ---*/
     for (unsigned j = 0; j < nCols; j++) {
         for (unsigned i = 0; i < nRows; i++) {
             res(i, j) = mtx[j*nRows+i] * rhs;
@@ -280,6 +315,7 @@ CMatrix CMatrix::operator*(const double& rhs) {
 CMatrix CMatrix::operator/(const double& rhs) {
     CMatrix res(nRows, nCols, 0.0);
 
+    /*--- Divide every entry with the constant. ---*/
     for (unsigned j = 0; j < nCols; j++) {
         for (unsigned i = 0; i < nRows; i++) {
             res(i, j) = mtx[j*nRows+i] / rhs;
@@ -292,6 +328,7 @@ CMatrix CMatrix::operator/(const double& rhs) {
 CMatrix CMatrix::operator^(const double& rhs) {
     CMatrix res(nRows, nCols, 0.0);
 
+    /*--- Take the power of every entry with the constant. ---*/
     for (unsigned j = 0; j < nCols; j++) {
         for (unsigned i = 0; i < nRows; i++) {
             res(i, j) = pow(mtx[j*nRows+i], rhs);
