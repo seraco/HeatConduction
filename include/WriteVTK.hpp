1cabf7886301246891ac5f12fb23048f84c9c673
seraco <sebastiaramon_1994@hotmail.com>
2018-03-22 10:44:27 +0100

    WriteVTK with comments

--- include/WriteVTK.hpp
+++ include/WriteVTK.hpp
@@ -1,3 +1,31 @@
+/*!
+ * @file WriteVTK.hpp
+ * @brief Headers of the main subroutines for writing VTK files.
+ *        The implementation is in the <i>WriteVTK.cpp</i> file.
+ * @author S.Ramon (seraco)
+ * @version 0.0.1
+ *
+ * Copyright 2018 S.Ramon
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef __CWRITEVTK_HPP
 #define __CWRITEVTK_HPP
 
@@ -7,7 +35,12 @@
 #include "CMesh.hpp"
 #include "CHeatConduction.hpp"
 
+/*!
+ * @brief Subroutine that writes VTK files.
+ * @param[in] fname - Filename.
+ * @param[in] msh - Mesh.
+ * @param[in] heat - Heat conduction with results.
+ */
 void writeVTK(std::string fname, const CMesh& msh, const CHeatConduction& heat);
-template <typename T> void swapEnd(T& var);
 
 #endif
--- src/WriteVTK.cpp
+++ src/WriteVTK.cpp
@@ -1,3 +1,30 @@
+/*!
+ * @file WriteVTK.cpp
+ * @brief The main subroutines for defining matrices and their operations.
+ * @author S.Ramon (seraco)
+ * @version 0.0.1
+ *
+ * Copyright 2018 S.Ramon
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef __CWRITEVTK_CPP
 #define __CWRITEVTK_CPP
 
@@ -10,8 +37,8 @@
 #include "../include/CMatrix.hpp"
 #include "../include/CHeatConduction.hpp"
 
-// TODO: Write in binary format!!
 void writeVTK(std::string fname, const CMesh& msh, const CHeatConduction& heat) {
+    /*--- Initialize variable to be used in the subroutine. ---*/
     unsigned nNod = msh.getNNode();
     unsigned nEle = msh.getNElem();
     unsigned nNodPerEle = msh.getNNodePerElem();
@@ -19,25 +46,25 @@ void writeVTK(std::string fname, const CMesh& msh, const CHeatConduction& heat) 
     CMatrix conn = msh.getConnMtx();
     CMatrix temp = heat.getTemp();
     double zero = 0.0;
+
+    /*--- Open file for writing results. ---*/
     std::ofstream vtk;
     vtk.open(fname, std::ios::out | std::ios::binary);
     if (vtk) {
+        /*--- Write header. ---*/
         std::cout << "\nWriting VTK file...\n\n";
         vtk << "# vtk DataFile Version 2.0" << "\n";
         vtk << "vtk output" << "\n";
         vtk << "ASCII" << "\n";
         vtk << "DATASET UNSTRUCTURED_GRID" << "\n";
+
+        /*--- Write coordinates of points. ---*/
         vtk << "POINTS " << nNod <<" double" << "\n";
         for (unsigned int i = 0; i < nNod; i++) {
             vtk << coor(i, 0) << " " << coor(i, 1) << " " << zero << "\n";
-            // swapEnd(coor(i, 0));
-            // vtk.write((char*)&coor(i, 0), sizeof(double));
-            // swapEnd(coor(i, 1));
-            // vtk.write((char*)&coor(i, 1), sizeof(double));
-            // swapEnd(zero);
-            // vtk.write((char*)&zero, sizeof(double));
         }
-        // vtk << "\n";
+
+        /*--- Write connectivity data. ---*/
         vtk << "CELLS " << nEle <<" " << nEle * (nNodPerEle + 1) << "\n";
         for (unsigned int i = 0; i < nEle; i++) {
             vtk << nNodPerEle;
@@ -46,10 +73,14 @@ void writeVTK(std::string fname, const CMesh& msh, const CHeatConduction& heat) 
             }
             vtk << "\n";
         }
+
+        /*--- Write the type of elements. Quads (9) in this case. ---*/
         vtk << "CELL_TYPES " << nEle << "\n";
         for (unsigned int i = 0; i < nEle; i++) {
             vtk << 9 << "\n";
         }
+
+        /*--- Write temperature results. ---*/
         vtk << "POINT_DATA " << nNod << "\n";
         vtk << "FIELD FieldData 1\n";
         vtk << "disp 1 "<< nNod << " double\n";
@@ -59,15 +90,6 @@ void writeVTK(std::string fname, const CMesh& msh, const CHeatConduction& heat) 
     vtk.close();
     } else {
         throw std::runtime_error("Problem writing VTK file");
-    }
-}
-
-template <typename T>
-void swapEnd(T& var) {
-    char* varArray = reinterpret_cast<char*>(&var);
-
-    for (long i = 0; i < static_cast<long>(sizeof(var) / 2); i++) {
-        std::swap(varArray[sizeof(var) - 1 - i], varArray[i]);
     }
 }
 
